c05d93215e2d6af544c653bcf765d6677bb7911d
Ismael <ismael@capptains.com>
2017-06-28 09:50:24 +0200

    basic structure

---
+++ app/src/main/java/es/florit/kotlin/notification/MainActivity.kt
@@ -0,0 +1,17 @@
+package es.florit.kotlin.notification
+
+import android.os.Bundle
+import android.support.annotation.Nullable
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by ismael on 28/6/17.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(@Nullable savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
